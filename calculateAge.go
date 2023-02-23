9f275b2988a5087607f62a4a8607d81875b4e473
Aorts <t.sappalunn@gmail.com>
2023-02-24 02:35:10 +0700

    create: get serviceStartDate

--- calculateAge.go
+++ calculateAge.go
@@ -5,17 +5,14 @@ import (
 	"time"
 )
 
-type Response struct {
-	eligibleFlag     string
-	serviceStartDate string
-	serviceEndDate   string
-}
-
 func calculate(birthDateString string) bool {
+
+	// declare var
 	birthDate, _ := time.Parse("2006/01/02", birthDateString)
 	startDate, _ := time.Parse("2006/01/02", "2021/06/01")
 	endDate, _ := time.Parse("2006/01/02", "2021/08/31")
-	//age := time.Since(birthDate).Hours() / 24 / 365
+
+	//get Age at first date and last date of Vaccine
 	ageAtStartDate := startDate.Sub(birthDate).Hours() / 24 / 365
 	ageAtEndDate := endDate.Sub(birthDate).Hours() / 24 / 365
 	//fmt.Println(ageAtStartDate)
@@ -30,7 +27,47 @@ func calculate(birthDateString string) bool {
 	return false
 }
 
+func getStartDate(birthDateString string) string {
+	//res := "2021/06/01"
+	// declare var
+	birthDate, _ := time.Parse("2006/01/02", birthDateString)
+	startDate, _ := time.Parse("2006/01/02", "2021/06/01")
+	endDate, _ := time.Parse("2006/01/02", "2021/08/31")
+	res := startDate.Format("2006/01/02")
+
+	//get Age at first date and last date of Vaccine
+	ageAtStartDate := startDate.Sub(birthDate).Hours() / 24 / 365
+	ageAtEndDate := endDate.Sub(birthDate).Hours() / 24 / 365
+
+	//fmt.Println(ageAtStartDate)
+	if (ageAtStartDate < 65 && ageAtEndDate >= 65) || ((ageAtStartDate < 0.5 && ageAtEndDate >= 0.5) && ageAtEndDate < 2) {
+		if ageAtStartDate < 65 && ageAtEndDate >= 65 {
+			birthDate = birthDate.AddDate(65, 0, 0)
+			res = birthDate.Format("2006/01/02")
+		} else if (ageAtStartDate < 0.5 && ageAtEndDate >= 0.5) && ageAtEndDate < 2 {
+			res = startDate.Format("2006/01/02")
+		}
+	}
+	return res
+}
+
+func Results(gender string, birthDateString string) (string, string, string) {
+	eligibleFlag := "N"
+	serviceStartDate := ""
+	serviceEndDate := ""
+
+	isCanVac := calculate(birthDateString)
+	if isCanVac {
+		eligibleFlag = "Y"
+		serviceStartDate = getStartDate(birthDateString)
+	}
+
+	return eligibleFlag, serviceStartDate, serviceEndDate
+}
+
 func main() {
-	aage := calculate("1956/09/01")
-	fmt.Println(aage)
+	eligibleFlag, serviceStartDate, serviceEndDate := Results("man", "2021/01/05")
+	fmt.Println("eligible Flag = ", eligibleFlag)
+	fmt.Println("service Start Date = ", serviceStartDate)
+	fmt.Println("service End Date = ", serviceEndDate)
 }
